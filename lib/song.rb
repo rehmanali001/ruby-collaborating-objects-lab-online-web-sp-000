ec701aa10b8c4afe1a6f2d1238ec94c47450b0b5
Rehman Ali <rehmanali001@gmail.com>
2019-08-01 21:11:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,13 @@ class Song
     @@all
   end
   
-  def self.new_by_filename(file_name)
-    new_file_name = file_name.split(" - ")
-    new_file = self.new(file_name)
-  end
+  def self.new_by_filename(file)
+    file_split_array = file.split(" - ")
+    song = self.new(file_split_array[1])
+    song_artist = Artist.find_or_create_by_name(file_split_array[0])
+    song.artist = song_artist
+    song_artist.add_song(song)
+    song
+
+   end
 end 
